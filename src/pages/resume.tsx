db473107180c4b8b953e79b9163623aff00e44f5
louay ghanney <louayghanney71@gmail.com>
2023-12-16 01:12:36 +0100

    fix resume bug

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -2,10 +2,7 @@ export default function Resume() {
     return (
         <div className="h-[calc(100svh-200px)] md:h-[calc(100svh-120px)] w-full">
             <h2 className="my-5 text-center text-3xl">My Resume</h2>
-            <iframe
-                className="h-full w-full"
-                src="https://www.louay.tn/assets/docs/cv_english.pdf"
-            />
+            <iframe className="h-full w-full" src="https://www.louay.tn/resume.pdf" />
         </div>
     );
 }
