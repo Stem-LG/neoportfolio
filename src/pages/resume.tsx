e1942141b98aa244b498a2618a5fdadcb3790840
louay ghanney <louayghanney71@gmail.com>
2023-12-24 18:50:25 +0100

    Update resume page styling

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -1,6 +1,6 @@
 export default function Resume() {
     return (
-        <div className="h-[calc(100svh-200px)] md:h-[calc(100svh-120px)] w-full">
+        <div className="h-[calc(100svh-200px)] md:h-[calc(100svh-160px)] w-full px-5">
             <h2 className="my-5 text-center text-3xl">My Resume</h2>
             <iframe className="h-full w-full" src="https://www.louay.tn/resume.pdf" />
         </div>
